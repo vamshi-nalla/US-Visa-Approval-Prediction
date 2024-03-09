0f1a94a079d60a28a25e41afc4a18101ade558d0
Vamshi-nalla <nvkrishna9396@gmail.com>
2024-03-10 01:08:52 +0530

    updated All

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vamshi_nalla",
+    author_email="nvkrishna9396@gmail.com",
+    packages=find_packages()
+)
